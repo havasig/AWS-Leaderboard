cf927acf428014902128d2acaa9e8ad8ffa9e162
Gabor Havasi <havasi.gaabor@gmail.com>
2026-02-23 19:00:12 +0100

    Add data store

---
+++ android/app/src/main/java/hu/havasig/awsleaderboard/domain/datastore/TokenDataStore.kt
@@ -0,0 +1,41 @@
+package hu.havasig.awsleaderboard.domain.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+@Singleton
+class TokenDataStore @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+
+    val token: Flow<String?> = context.dataStore.data
+        .map { preferences -> preferences[TOKEN_KEY] }
+
+    suspend fun saveToken(token: String) {
+        context.dataStore.edit { preferences ->
+            preferences[TOKEN_KEY] = token
+        }
+    }
+
+    suspend fun clearToken() {
+        context.dataStore.edit { preferences ->
+            preferences.remove(TOKEN_KEY)
+        }
+    }
+
+    companion object {
+
+        private val TOKEN_KEY = stringPreferencesKey("jwt_token")
+    }
+}
